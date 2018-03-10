e5140344843ab481146fa801f4c7f8c21cb49579
unknown <cyril.caron95@gmail.com>
2018-03-10 17:42:51 +0100

    feat(comments) : add comments to cash-> index.js

--- cash/bin/index.js
+++ cash/bin/index.js
@@ -6,9 +6,22 @@ const cash = require('./cash.js');
 
 const config = new Conf();
 
+/**
+*@param  {array} argv - retrieve the command line arguments to construct the command given to cash module
+*slice(2) return everything that was typed in the command line.
+*If it was slice(0) , argv[0] would be the path to nodeJS and argv[1] would be the path to the script
+*/
+
 const argv = process.argv.slice(2);
 
 helpers(argv);
+
+/**
+*@param {Object} command - construct the command object to pass to cash from the argv array
+*@property {number} amount - the amount to Convert
+*@property {string} from - the currency to convert from
+*@property {string} to - the currency to convert to
+*/
 
 const command = {
   'amount': argv[0] || 1,
@@ -18,5 +31,7 @@ const command = {
       ? process.argv.slice(4)
       : config.get('defaultTo', ['USD', 'EUR', 'GBP'])
 };
+
+/** call the cash module with the command as argument */
 
 cash(command);
